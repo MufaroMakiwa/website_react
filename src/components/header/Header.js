7966c52796fecc0b105ed43c8636412ec4447c51
Mufaro Makiwa <mmakiwa@mit.edu>
2021-07-01 09:49:59 -0700

    Creates header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Header.css";
+import header_logo from "../../assets/header_logo.png";
 
 const Header = () => {
   return (
-    <div className="">
-
+    <div className="main-container header-container">
+      <img src={header_logo} alt="Header Logo" className="header-logo"></img>
     </div>
   )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Home.css";
+import Header from "../../components/header/Header"
 
 const Home = () => {
   return (
-    <div>
+    <div className="main-container">
+      <Header />
       HOME
     </div>
   )
